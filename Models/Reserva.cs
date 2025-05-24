b29ef496246b04d614d9d00b2ae551125dc6364c
Gabriel Galacci <gabrielcssoad@hotmail.com>
2025-05-24 16:28:24 -0300

    Criando Esqueleto das Classes

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -7,6 +7,23 @@ namespace ProjetoHospedagem.Models
 {
     public class Reserva
     {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> pessoas) { }
+
+        public void CadastrarSuite(Suite suite) { }
+
+        public int ObterQuantidadeDeHospedes() { return 0; }
         
+        public decimal CalcularValorHospedagem(){ return 1; }
     }
 }
